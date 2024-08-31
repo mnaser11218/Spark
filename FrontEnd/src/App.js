6c7901e4213331a8b5e6a69297bc24abe2e50762
Mohammed <mohammed@Zipcoders-MacBook-Pro-9.local>
2024-08-30 20:28:23 -0400

    added spark component

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import HomePage from './components/HomePage';
-
+import Spark from './components/Spark'
 
 const App =()=> {
   return (
-  
+
 <Router>
   <Routes>
-    <Route path="/" element={<HomePage />} /> 
-    <Route path="/login" element={<Login />} /> 
-  </Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/spark" element={<Spark />} />
+   </Routes>
   </Router>
 
   );
