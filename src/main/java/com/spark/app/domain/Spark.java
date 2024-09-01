10c9ee1b1ffd44c92bbd6da8b6a17b08eaf3cd6d
Mohammed <mohammed@Zipcoders-MacBook-Pro-9.local>
2024-09-01 14:34:36 -0400

    created getuserbyusername method in userprofile repo, and added endpoint to get users by username in userprofile controller

--- src/main/java/com/spark/app/domain/Spark.java
+++ src/main/java/com/spark/app/domain/Spark.java
@@ -19,7 +19,6 @@ import org.hibernate.annotations.CacheConcurrencyStrategy;
 public class Spark implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
