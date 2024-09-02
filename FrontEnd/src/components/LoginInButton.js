6a29648396a6a7a74c54f1e2506ed1102e7ed2f2
Bryan <bryan@Zipcoders-MacBook-Pro-10.local>
2024-09-02 11:11:51 -0400

    BryanAndMo's Debugging adventure added METHOD getSparksByUserName

--- FrontEnd/src/components/LoginInButton.js
+++ FrontEnd/src/components/LoginInButton.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../componentStyles/HomeStyle.css';
+
 
 
 const LoginInButton = () => {
@@ -14,7 +16,7 @@ const LoginInButton = () => {
 
   return (
     <div >
-      <button onClick={handleClick}>Log In</button>
+      <button className="btn btn-primary" onClick={handleClick}>Log In</button>
       <br/>
       
     
