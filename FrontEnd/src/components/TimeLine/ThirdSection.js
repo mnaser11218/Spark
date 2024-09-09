4e0771f1b99cd85b95426f9aa1708507686235df
Josue <josue@Zipcoders-MacBook-Pro-75.local>
2024-09-09 17:05:44 -0400

    styling complete and third sectino complete

--- FrontEnd/src/components/TimeLine/ThirdSection.js
+++ FrontEnd/src/components/TimeLine/ThirdSection.js
@@ -3,9 +3,10 @@ import DisplaySearchBar from '../ExplorePage/SearchBar';
 import './ThirdSection.css';
 import { useNavigate } from 'react-router';
 import SparkPage from '../Spark/SparkPage';
+import imageForNews from '../Images/hq720 (1).jpg';
+import imageForNews2 from '../Images/9E0KvZb.webp';
 
 function ShowThirdSection (){
-
 
 
 const navigate = useNavigate();
@@ -19,6 +20,12 @@ function handleClickAction(){
 <div id="third-div-section">
   <h1 id="discover-sparks-h1">Discover Sparks</h1>
   <DisplaySearchBar/>
+
+  <div>
+    <h3 id="whats-trending">What's Trending?</h3>
+    <img src={imageForNews} id="the-image-for-news"/>
+    <img src={imageForNews2} id="the-image-for-news-2"/>
+  </div>
   {/* <div>
     <button onClick={handleClickAction}>Press me</button>
   </div> */}
